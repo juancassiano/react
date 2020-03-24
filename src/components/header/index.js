cda524e884aadf42738b9adfdb481ad4b30a3831
jcassiano <juancassiano@hotmail.com>
2020-03-24 01:09:08 -0300

    Construcao de pagina JSHUNT com React - Rocketseat

---
+++ src/components/header/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './style.css';
+
+//class Header extends components{render(){return...}}
+const Header = () =>  <header id="main-header">JSHunt</header>
+
+export default Header;
